d92a7cfd3769bff4d6e1589465dbf29daf2fb7ea
ashujohri <ashutoshjohri5@gmail.com>
2025-07-08 18:16:52 +0530

    update password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://NamasteDevNodeJS:AmhmbhVuYAGMGX3P@namastenode.joqzp.mongodb.net/devTinder"
+    "mongodb+srv://NamasteDevNodeJS:sf0bajigZFYjgeHy@namastenode.joqzp.mongodb.net/devTinder"
   );
 };
 
